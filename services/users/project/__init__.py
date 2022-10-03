c04b2cd9f57563de26d8d463a58e0f3ce237fdf3
Phil Zona <pzona@launchdarkly.com>
2022-10-03 09:12:31 -0400

    Modularize app for testing

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,35 +1,22 @@
 import os
-from dotenv import load_dotenv
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-load_dotenv()
+db = SQLAlchemy()
 
-app = Flask(__name__)
-api = Api(app)
+def create_app(script_info=None):
+    app = Flask(__name__)
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
+    db.init_app(app)
 
-db = SQLAlchemy(app)
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-class UsersPing(Resource):
-    def get(self):
-        return {
-        'status': 'success',
-        'message': 'pong!'
-    }
-
-api.add_resource(UsersPing, '/users/ping')
+    return app
